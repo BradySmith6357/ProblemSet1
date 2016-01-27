b5b743cbe587937e0866b758449940f5889cf066
Student <student@RU-023.local>
2016-01-27 15:35:30 -0700

    Worked on solving problem. Still needs work.

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 var letterCapitalize = function (str){
-	return str
-		.split(" ");
-		
+		var words = str.split(" ");
+		var arr = Array();
+		for (i in words) {
+			temp = words[i].toLowerCase();
+			temp = temp.charAt(0).toUpperCase() + temp.substring(1);
+			arr.push(temp);
+		}
+		return arr.join
+		console.log(str)
 }
-console.log(letterCapitalize("first letter should be capatalized"))
+		
+letterCapitalize("This is a string")
 
+// 2nd possible solution
+
+// var letterCapitalize = function (str){
+// 	return str
+// 		var splitArray = .split(" ");
+// 		var capLetters = function (splitArray) {
+// 			var output = wordsArray.map(function(element){
+// 				return capOneWord(element)
+// 	})
+// 	return output
+// }
+
+// console.log(letterCapitalize("first letter should be capatalized"))
+
+//  3rd possible solution
+// var capOneWord = function(oneWord){
+// 	return oneWord.toUpperCase()
+// }
+// var mapCallback = function(element){
+// 	return wordsArray.map(mapCallback)
+// }
+// var capitalizedWords = capOneWords(words)
+// console.log(capitalizedWords)
